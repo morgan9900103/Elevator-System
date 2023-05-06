9c7aee7d3cbd86495675ce099db0e6dc6bfa577b
Morgan Lee <morgan9900103@gmail.com>
2023-05-05 22:27:04 -0400

    Add add-elevator feature

--- src/system.cpp
+++ src/system.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 #include "system.h"
 
@@ -11,6 +12,8 @@ void System::Usage() {
   std::cout << "\t- enter-elevator <elevator-id> <total-weight-in-kgs-entering> <space-seperated-floor-names-requested>" << std::endl;
   std::cout << "\t- exit-elevator <elevator-id> <weight-exiting>" << std::endl;
   std::cout << "\t- continue <elevator-id>" << std::endl;
+  std::cout << "\t- quit" << std::endl;
+  std::cout << "-------------------------------------------------------------------------------" << std::endl;
 }
 
 // Add an elevator to the system
@@ -26,4 +29,29 @@ bool System::AddElevator(Elevator* elevator) {
 // main function
 void System::Run() {
   Usage();
+
+  std::string input;
+  while (true) {
+    std::cout << "\nPlease enter your input:" << std::endl;
+    std::getline(std::cin, input);
+    std::stringstream inputstream(input);
+    std::string command;
+    inputstream >> command;
+    if (command == "quit") {
+      std::cout << "\nExiting the simulator" << std::endl;
+      break;
+    }
+    else {
+      if (command == "add-elevator") {
+        std::string elevator_id, weight;
+        inputstream >> elevator_id >> weight;
+        Elevator* elevator = new Elevator(elevator_id, std::stoi(weight));
+        if (!AddElevator(elevator)) {
+          std::cout << "Invalid elevator. Please enter again" << std::endl;
+        } else {
+          std::cout << "Add elevator " << elevator_id << " with " << weight << " kg success. " << std::endl;
+        }
+      }
+    }
+  }
 }
